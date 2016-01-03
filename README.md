3e28b5ca537d7632411c9f232261c7626114beab
Greg Raven <greg@corax.org>
2016-01-03 14:50:04 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test-suite
+
+Various HTML, PHP, and SSI files for checking real-world performance of web hosts
+
+Upload these files to a web host, and then test the speed of your host using GTMetrix, WebPageTest, or plain old Apache Bench.
---
+++ php/php-info.php
@@ -0,0 +1,12 @@
+<?php
+/*------------------------------------------------------------------------------------------
+     Generate PHP info page for the server on which this is running
+
+     $Revision: 1.0 $
+     $Date: 2012/01/23 $
+     $Author: Kevin McCormick $
+		 $Website: http://localhost $
+
+--------------------------------------------------------------------------------------------*/
+	phpinfo();
+?>
