8c78c0099334e7050ea06ee591213c1691e2ced5
Greg Raven <greg@corax.org>
2016-03-07 08:34:15 -0800

    first draft complete

---
+++ php/php-local-combined.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>PHP with combined local resource calls</title>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_assets/includes/meta-local-combined.htm'; ?>
+</head>
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_assets/includes/page-content.htm'; ?>
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_assets/includes/js-local-combined.htm'; ?>
+</body>
+</html>
